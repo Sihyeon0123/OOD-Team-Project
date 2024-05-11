4b83661f0d4f9552d5dd1641a4da577fc4b83fad
wkdgydms9121 <84010233+wkdgydms9121@users.noreply.github.com>
2024-05-12 01:30:12 +0900

    보낸 메일함 table 추가
    MessageFormatter.java

--- src/main/java/deu/cse/spring_webmail/repository/SentEmailRepository.java
+++ src/main/java/deu/cse/spring_webmail/repository/SentEmailRepository.java
@@ -1,6 +1,5 @@
 package deu.cse.spring_webmail.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
